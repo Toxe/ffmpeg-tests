1a899749d0d01e5d348e6d0bfd6732d70349e0eb
Toxe <toxe@toxe.net>
2021-10-06 11:10:52 +0200

    convert_screenshots.sh

---
+++ convert_screenshots.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+convert -size 1920x1080 -depth 8 rgb:screenshot_000.raw screenshot_000.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_001.raw screenshot_001.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_002.raw screenshot_002.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_003.raw screenshot_003.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_004.raw screenshot_004.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_005.raw screenshot_005.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_006.raw screenshot_006.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_007.raw screenshot_007.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_008.raw screenshot_008.png
+convert -size 1920x1080 -depth 8 rgb:screenshot_009.raw screenshot_009.png
