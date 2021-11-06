efb5037c4bd5f4370dd3adf0ee85aa9146a6b2eb
Toxe <toxe@toxe.net>
2021-11-06 12:58:06 +0100

    Store filename and format

--- src/threaded_stream_reader/video/video_file.cpp
+++ src/threaded_stream_reader/video/video_file.cpp
@@ -18,11 +18,15 @@ int VideoFile::open_file(const std::string_view& full_filename)
     if (!std::filesystem::exists(path))
         return show_error("file not found");
 
+    filename_without_path_ = path.filename().string();
+
     // allocate format context
     format_context_ = factory_->create_format_context(full_filename);
 
     if (!format_context_)
         return -1;
+
+    file_format_ = format_context_->format();
 
     // find best audio and video stream
     audio_stream_info_ = format_context_->find_best_stream(factory_, FormatContext::StreamType::audio);
--- src/threaded_stream_reader/video/video_file.hpp
+++ src/threaded_stream_reader/video/video_file.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <string>
 #include <string_view>
 
 #include "adapters/format_context/format_context.hpp"
@@ -17,12 +18,18 @@ class VideoFile {
 
     bool is_open_ = false;
 
+    std::string filename_without_path_;
+    std::string file_format_;
+
     int open_file(const std::string_view& full_filename);
 
 public:
     VideoFile(const std::string_view& full_filename, Factory* factory);
 
     [[nodiscard]] bool is_open() const { return is_open_; }
+
+    [[nodiscard]] const std::string& filename() const { return filename_without_path_; }
+    [[nodiscard]] const std::string& file_format() const { return file_format_; }
 
     [[nodiscard]] StreamInfo* audio_stream_info() const { return audio_stream_info_.get(); }
     [[nodiscard]] StreamInfo* video_stream_info() const { return video_stream_info_.get(); }
