c9bb43c81060ef5daba8992bf9bd7281c1a72d54
Toxe <toxe@toxe.net>
2022-11-08 19:19:18 +0100

    Stream does not always have a valid duration

--- src/dump_last_frames/show_info.cpp
+++ src/dump_last_frames/show_info.cpp
@@ -18,19 +18,24 @@ void print_info(const AVFormatContext* format_context, const AVCodecContext* cod
     static_assert(AV_TIME_BASE == 1'000'000);
 
     const AVStream* stream = format_context->streams[stream_index];
-    const double stream_time_base = av_q2d(stream->time_base);
 
-    const std::chrono::duration<double> pos{static_cast<double>(pts) * stream_time_base};
-    const std::chrono::duration<double> dur{static_cast<double>(pkt_duration) * stream_time_base};
-    const std::chrono::duration<double> stream_duration = std::chrono::microseconds{format_context->duration};
+    const std::chrono::duration<double> pos{static_cast<double>(pts) * av_q2d(stream->time_base)};
+    const std::chrono::duration<double> dur{static_cast<double>(pkt_duration) * av_q2d(stream->time_base)};
+    const std::chrono::duration<double> format_context_duration = std::chrono::microseconds{format_context->duration};
+
+    // stream duration: either stored in stream or estimated
+    int64_t stream_duration = stream->duration;
+
+    if (stream_duration == INT64_MIN)
+        stream_duration = (format_context->duration * stream->time_base.den) / (stream->time_base.num * static_cast<int64_t>(AV_TIME_BASE));
 
     fmt::print("[{:.3f}s | {:.3f}s] {} {} {} time_base: {}/{}, duration: {} ({:.5f}s), pts: {} / {} (-{})\n",
-        pos.count(), stream_duration.count(),
+        pos.count(), format_context_duration.count(),
         av_get_media_type_string(codec_context->codec_type),
         object_type, video_frame_type,
         stream->time_base.num, stream->time_base.den,
         pkt_duration, dur.count(),
-        pts, stream->duration, stream->duration - pts);
+        pts, stream_duration, stream_duration - pts);
 }
 
 void show_frame_info(const AVFormatContext* format_context, const AVCodecContext* codec_context, const int stream_index, const AVFrame* frame)
